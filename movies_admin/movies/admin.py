6d108383a6bd274a5577107f069029d5c829f4c9
oksanaleonova <loksana864@gmail.com>
2026-05-02 18:31:40 +0300

    feature: register entity

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -1,7 +1,56 @@
 from django.contrib import admin
-from .models import Genre
+
+from .models import (
+    Genre,
+    GenreFilmWork,
+    FilmWork,
+    Person,
+    PersonFilmWork
+)
+
+
+class GenreFilmWorkInline(admin.TabularInline):
+    model = GenreFilmWork
+
+
+class PersonFilmWorkInline(admin.TabularInline):
+    model = PersonFilmWork
+
+
+@admin.register(FilmWork)
+class FilmWorkAdmin(admin.ModelAdmin):
+    inlines = (
+        GenreFilmWorkInline,
+        PersonFilmWorkInline,
+    )
+
+    list_display = (
+        'title',
+        'type',
+        'creation_date',
+        'rating',
+    )
+
+    search_fields = ('title',)
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('full_name',)
+
+    search_fields = ('full_name',)
+
+
+@admin.register(GenreFilmWork)
+class GenreFilmWorkAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PersonFilmWork)
+class PersonFilmWorkAdmin(admin.ModelAdmin):
     pass
